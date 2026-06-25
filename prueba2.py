93e3f14e16e00935cf226dbf11d10527c5970585
Leonel <leonelparrales22@gmail.com>
2026-06-25 14:42:22 -0500

    Implement pyspark_output function for Kafka integrationprueba2.py

---
+++ prueba2.py
@@ -0,0 +1,93 @@
+import re
+import uuid
+
+
+def pyspark_output(spark, df, write_options_dict, param_dict):
+
+    topic_tango = "{{{Transf_PRY_PaymentHub.P_TOPICO_SINCRONIZACION_TANGO}}}"
+
+    kafka_auth = "{{{Transf_PRY_PaymentHub.P_KAFKA_AUTH}}}"
+    usr_match = re.search(r"username=[\"']([^\"']+)[\"']", kafka_auth)
+    pwd_match = re.search(r"password=[\"']([^\"']+)[\"']", kafka_auth)
+
+    kafka_conf_vals = {
+        "bootstrap.servers": "{{{Transf_PRY_PaymentHub.P_KAFKA_HOST}}}:{{{Transf_PRY_PaymentHub.P_KAFKA_PORT}}}",
+        "sasl.username": usr_match.group(1) if usr_match else "",
+        "sasl.password": pwd_match.group(1) if pwd_match else "",
+        "sr.url": "{{{Transf_PRY_PaymentHub.P_SCHEMA_REGISTRY_URL}}}",
+        "sr.auth": "{{{Transf_PRY_PaymentHub.P_SCHEMA_REGISTRY_AUTH}}}",
+    }
+
+    global schema_tango_cache
+    if "schema_tango_cache" not in globals():
+        from confluent_kafka.schema_registry import SchemaRegistryClient
+
+        sr_client_driver = SchemaRegistryClient(
+            {
+                "url": kafka_conf_vals["sr.url"],
+                "basic.auth.user.info": kafka_conf_vals["sr.auth"],
+            }
+        )
+        schema_tango_cache = sr_client_driver.get_latest_version(
+            f"{topic_tango}-value"
+        ).schema.schema_str
+
+    def send_partition(partition):
+        from confluent_kafka import Producer
+        from confluent_kafka.schema_registry import SchemaRegistryClient
+        from confluent_kafka.schema_registry.avro import AvroSerializer
+        from confluent_kafka.serialization import SerializationContext, MessageField
+
+        sr_client_exec = SchemaRegistryClient(
+            {
+                "url": kafka_conf_vals["sr.url"],
+                "basic.auth.user.info": kafka_conf_vals["sr.auth"],
+            }
+        )
+
+        avro_serializer_tango = AvroSerializer(
+            sr_client_exec,
+            schema_tango_cache,
+            conf={"auto.register.schemas": False},
+        )
+        ctx_tango = SerializationContext(topic_tango, MessageField.VALUE)
+
+        p = Producer(
+            {
+                "bootstrap.servers": kafka_conf_vals["bootstrap.servers"],
+                "security.protocol": "SASL_SSL",
+                "sasl.mechanism": "PLAIN",
+                "sasl.username": kafka_conf_vals["sasl.username"],
+                "sasl.password": kafka_conf_vals["sasl.password"],
+                "client.id": f"pyspark-producer-{uuid.uuid4()}",
+                "enable.idempotence": True,
+                "linger.ms": 5,
+                "compression.type": "lz4",
+                "acks": "all",
+            }
+        )
+
+        for row in partition:
+            record = row.asDict(recursive=True)
+            message_key = (
+                str(record["id_transaccion"])
+                if "id_transaccion" in record
+                else str(uuid.uuid4())
+            )
+
+            kafka_payload = record["kafka_message"]
+
+            value_bytes_tango = avro_serializer_tango(kafka_payload, ctx_tango)
+            p.produce(
+                topic=topic_tango,
+                key=message_key.encode("utf-8"),
+                value=value_bytes_tango,
+            )
+
+            p.poll(0)
+        p.flush()
+
+    df.rdd.foreachPartition(send_partition)
+
+
+
