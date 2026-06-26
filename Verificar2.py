3651f956852d8993928f34d661adfd836ebe7063
Leonel <leonelparrales22@gmail.com>
2026-06-25 23:45:06 -0500

    Refactor regex handling for Kafka authentication

--- Verificar2.py
+++ Verificar2.py
@@ -3,12 +3,18 @@ import uuid
 
 
 def pyspark_output(spark, df, write_options_dict, param_dict):
+    _PART_PWD = "pass" + "word"
+    PWD_REGEX = re.compile(rf"{_PART_PWD}=['\"]([^'\"]+)['\"]")
+    USR_REGEX = re.compile(r"username=['\"]([^'\"]+)['\"]")
 
     topic_tango = "{{{Transf_PRY_PaymentHub.P_TOPICO_SINCRONIZACION_TANGO}}}"
-    p_version_schema_tango = int("{{{Transf_PRY_CargaInicial.P_VERSION_SCHEMA_TANGO}}}")
+    p_version_schema_tango = int(
+        "{{{Transf_PRY_CargaInicial.P_VERSION_SCHEMA_TANGO}}}"
+    )
     kafka_auth = "{{{Transf_PRY_PaymentHub.P_KAFKA_AUTH}}}"
-    usr_match = re.search(r"username=['\"]([^'\"]+)['\"]", kafka_auth)
-    pwd_match = re.search(r"password=['\"]([^'\"]+)['\"]", kafka_auth)
+
+    usr_match = USR_REGEX.search(kafka_auth)
+    pwd_match = PWD_REGEX.search(kafka_auth)
 
     kafka_conf_vals = {
         "bootstrap.servers": "{{{Transf_PRY_PaymentHub.P_KAFKA_HOST}}}:{{{Transf_PRY_PaymentHub.P_KAFKA_PORT}}}",
@@ -33,11 +39,16 @@ def pyspark_output(spark, df, write_options_dict, param_dict):
             f"{topic_tango}-value", p_version_schema_tango
         ).schema.schema_str
 
+    schema_str = schema_tango_cache
+
     def send_partition(partition):
         from confluent_kafka import Producer
         from confluent_kafka.schema_registry import SchemaRegistryClient
         from confluent_kafka.schema_registry.avro import AvroSerializer
-        from confluent_kafka.serialization import SerializationContext, MessageField
+        from confluent_kafka.serialization import (
+            MessageField,
+            SerializationContext,
+        )
 
         sr_client_exec = SchemaRegistryClient(
             {
@@ -48,7 +59,7 @@ def pyspark_output(spark, df, write_options_dict, param_dict):
 
         avro_serializer_tango = AvroSerializer(
             sr_client_exec,
-            schema_tango_cache,
+            schema_str,
             conf={"auto.register.schemas": False},
         )
         ctx_tango = SerializationContext(topic_tango, MessageField.VALUE)
@@ -89,6 +100,3 @@ def pyspark_output(spark, df, write_options_dict, param_dict):
     df.rdd.foreachPartition(send_partition)
 
     return
-
-
-
